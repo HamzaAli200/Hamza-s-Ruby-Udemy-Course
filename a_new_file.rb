a2076c933a472baaf779cc28da1039d373669151
HamzaAli200 <alie.200@hotmail.com>
2019-02-17 13:57:48 -0500

    delete files

--- a_new_file.rb
+++
@@ -1,5 +0,0 @@
-# this is a new file some code whatever
-
-puts  "Hello Hamza"
-puts " You are doing greate job with ruby"
-
--- ruby_method.rb
+++
@@ -1,9 +0,0 @@
-def users
-  puts "Hello Ruby"
-end
-
-def user (first_name)
-  puts first_name
-end
-user "hamza"
-users
